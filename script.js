928d7df476bbab1584954f745550baa77a232a3f
Afsha <afsha@afterpaytouch.com>
2021-06-23 22:49:29 +1000

    setting local storage

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-console.log("enter js");
-
 var dateEl = $("#currentDay")
+var saveBtnEl = $('.saveBtn')
+var textboxEl = $('.textbox')
 
 var displayDate = moment().format("dddd, MMMM Do")
-console.log(displayDate)
 dateEl.text(displayDate)
+
+var task1 = localStorage.getItem("to-do")
+textboxEl.append(task1)
+
+function saveList(){
+    var listText = textboxEl.val();
+    console.log(listText)
+    localStorage.setItem("to-do", listText)
+}
+
+saveBtnEl.on('click', saveList)
