fa47ced5cd91d0bd6ff92e6ca98386839c6e443b
Afsha <afsha@afterpaytouch.com>
2021-06-28 21:24:49 +1000

    added set local storage function

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
 var dateEl = $("#currentDay")
 var saveBtnEl = $('.saveBtn')
 var textboxEl = $('.textbox')
+var timeEl = $('.time-block')
+
+var arrayList = []
+var tasks = {
+    "9": [],
+    "10": [],
+    "11": [],
+    "12": [],
+    "13": [],
+    "14": [],
+    "15": [],
+    "16": [],
+    "17": []
+};
 
 var displayDate = moment().format("dddd, MMMM Do")
 dateEl.text(displayDate)
 
+var currentTime = moment().format("HH")
+for(i=0; i<timeEl.length; i++){
+    if(timeEl[i].dataset.hour === Number(currentTime)){
+        textboxEl[i].className = "present"
+    }
+    if(timeEl[i].dataset.hour < Number(currentTime)){
+        textboxEl[i].className = "past"
+    }
+    if(timeEl[i].dataset.hour > Number(currentTime)){
+        textboxEl[i].className = "future"
+    }
+}
+
 var task1 = localStorage.getItem("to-do")
 textboxEl.append(task1)
 
-function saveList(){
-    var listText = textboxEl.val();
-    console.log(listText)
-    localStorage.setItem("to-do", listText)
+$('.container').on('click', '.saveBtn', save)
+
+function save(event){
+    
+    var taskInfo = $(event.target).closest(".first")
+    var textArea = taskInfo.find("textarea");
+
+    var time = taskInfo.attr("data-block")
+    var text = textArea.val().trim();
+    setTasks(time, text);
 }
 
-saveBtnEl.on('click', saveList)
+var setTasks = function(time, text) {
+    tasks[time] = [text];
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
